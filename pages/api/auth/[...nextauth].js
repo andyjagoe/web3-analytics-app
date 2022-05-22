c90dd75a73bf87f05c9015973bca57d7679cd000
Andy Jagoe <andy@jagoe.com>
2022-05-21 19:22:46 -0700

    add sign in with discord

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    DiscordProvider({
+        clientId: process.env.DISCORD_ID,
+        clientSecret: process.env.DISCORD_SECRET
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,

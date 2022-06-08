8ecec1bcf914bb2ed4a6982d57b8990053ef3edc
Andy Jagoe <andy@jagoe.com>
2022-06-07 17:09:24 -0700

    add temporary warning to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import {
   Typography,
-  } from '@mui/material'
+  Alert,
+  AlertTitle,
+  Container
+} from '@mui/material'
 import {useTheme} from '@mui/material/styles'
 import type { NextPage } from 'next'
 import Head from 'next/head'
@@ -32,6 +35,12 @@ const Home: NextPage = () => {
         Decentralized. Where users own their data.
       </Typography>
 
+      <Container maxWidth="xs">
+        <Alert severity="error" sx={{marginTop: theme.spacing(8)}}>
+            <AlertTitle>Caution</AlertTitle>
+            Active development. Expect data to be wiped.
+        </Alert>
+      </Container>
     </div>
   )
 }
